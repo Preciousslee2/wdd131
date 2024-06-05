d65d8bd8a8da2454cbf73e0509e3a1ae8db6315d
Preciousslee2 <Preciousslee65@gmail.com>
2024-06-05 22:30:20 +0100

    wdd131 added

--- week1/scripts/getdates.js
+++ week1/scripts/getdates.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded',function(){
     //set footer currentyear
-    document.getElementById('2024').textContent=new Date().getFullYear();
+    document.getElementById('currentyear').textContent=new Date().getFullYear();
     
     //set footer last modified date
     document.getElementById("lastModified").textContent=document.lastModified;
